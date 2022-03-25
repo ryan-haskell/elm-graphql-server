7d145384d8d78804adc951def3fec70cb60442a5
Ryan Haskell-Glatz <ryan.nhg@gmail.com>
2022-03-24 19:12:27 -0500

    remove unused location resolver

--- src/Resolvers/Post/Location.elm
+++
@@ -1,9 +0,0 @@
-module Resolvers.Post.Location exposing (resolver)
-
-import GraphQL.Response exposing (Response)
-import Schema.Post exposing (Post)
-
-
-resolver : Post -> () -> Response (Maybe String)
-resolver post args =
-    GraphQL.Response.ok post.location
